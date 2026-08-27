1b9d9cd2f0e80cd8d674dfac0056d7423c87f4f7
saifsaifulla15-crypto <285677039+saifsaifulla15-crypto@users.noreply.github.com>
2026-08-27 18:16:09 +0530

    Fix base url in frontend

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,2 +1,2 @@
 // We use the API Gateway on port 8765 to route our requests to the microservices.
-const API_BASE_URL = 'http://localhost:8765';
+const API_BASE_URL = 'https://virtual-queue-management-system-ko4q.onrender.com';
